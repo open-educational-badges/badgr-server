ce5993b6c488d1e1b64944f23ca734ee58e462eb
jona159 <raabejonas@yahoo.de>
2026-03-26 15:35:02 +0100

    fix: migration paths

---
+++ apps/issuer/migrations/0063_merge_all_0052_heads.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("issuer", "0052_auto_20251121_2108"),
+        ("issuer", "0052_badgeclass_language"),
+        ("issuer", "0062_alter_issuer_quota_network_period_start_and_more"),
+    ]
+
+    operations = []
