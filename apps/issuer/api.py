fc388b7baf149d363d358424956c6d3a9749f703
Stefan Trautvetter <stefan.trautvetter@esirion.de>
2026-04-27 17:26:18 +0200

    fix(batch): move shared_task to class to make it overridable

--- apps/issuer/api.py
+++ apps/issuer/api.py
@@ -724,84 +724,86 @@ class BadgeClassDetail(BaseEntityDetailView):
     def put(self, request, **kwargs):
         return super(BadgeClassDetail, self).put(request, **kwargs)
 
+class BadgeInstancesBatchAssertionTask:
+    def process_batch_assertions(
+        self,
+        assertions,
+        user_id,
+        badgeclass_id,
+        issuerSlug,
+        create_notification=False,
+    ):
+        try:
+            User = get_user_model()
+            user = User.objects.get(id=user_id)
+            badgeclass = BadgeClass.objects.get(id=badgeclass_id)
+
+            total = len(assertions)
+
+            processed = 0
+            successful = []
+            errors = []
+
+            for assertion in assertions:
+                request_entity_id = assertion.get("request_entity_id")
+                assertion["create_notification"] = create_notification
+
+                serializer = BadgeInstanceSerializerV1(
+                    data=assertion,
+                    context={
+                        "badgeclass": badgeclass,
+                        "user": user,
+                        "issuerSlug": issuerSlug,
+                    },
+                )
+
+                if serializer.is_valid():
+                    try:
+                        instance = serializer.save(created_by=user)
+                        successful.append(
+                            {
+                                "badge_instance": BadgeInstanceSerializerV1(instance).data,
+                                "request_entity_id": request_entity_id,
+                            }
+                        )
+                    except Exception as e:
+                        errors.append({"assertion": assertion, "error": str(e)})
+                else:
+                    errors.append({"assertion": assertion, "error": serializer.errors})
+
+                processed += 1
+
+                # Emit progress after each iteration
+                self.update_state(
+                    state="PROGRESS",
+                    meta={
+                        "processed": processed,
+                        "total": total,
+                        "data": successful,
+                        "errors": errors,
+                    },
+                )
+
+            return {
+                "success": len(errors) == 0,
+                "status": status.HTTP_201_CREATED
+                if len(errors) == 0
+                else status.HTTP_207_MULTI_STATUS,
+                "data": successful,
+                "errors": errors,
+            }
+
+        except Exception as e:
+            return {
+                "success": False,
+                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "error": str(e),
+            }
+
 
 @shared_task(bind=True)
-def process_batch_assertions(
-    self,
-    assertions,
-    user_id,
-    badgeclass_id,
-    issuerSlug,
-    serializer_class,
-    create_notification=False,
-):
-    try:
-        User = get_user_model()
-        user = User.objects.get(id=user_id)
-        badgeclass = BadgeClass.objects.get(id=badgeclass_id)
-
-        total = len(assertions)
-
-        processed = 0
-        successful = []
-        errors = []
-
-        for assertion in assertions:
-            request_entity_id = assertion.get("request_entity_id")
-            assertion["create_notification"] = create_notification
-
-            serializer = serializer_class(
-                data=assertion,
-                context={
-                    "badgeclass": badgeclass,
-                    "user": user,
-                    "issuerSlug": issuerSlug,
-                },
-            )
-
-            if serializer.is_valid():
-                try:
-                    instance = serializer.save(created_by=user)
-                    successful.append(
-                        {
-                            "badge_instance": serializer_class(instance).data,
-                            "request_entity_id": request_entity_id,
-                        }
-                    )
-                except Exception as e:
-                    errors.append({"assertion": assertion, "error": str(e)})
-            else:
-                errors.append({"assertion": assertion, "error": serializer.errors})
-
-            processed += 1
-
-            # Emit progress after each iteration
-            self.update_state(
-                state="PROGRESS",
-                meta={
-                    "processed": processed,
-                    "total": total,
-                    "data": successful,
-                    "errors": errors,
-                },
-            )
-
-        return {
-            "success": len(errors) == 0,
-            "status": status.HTTP_201_CREATED
-            if len(errors) == 0
-            else status.HTTP_207_MULTI_STATUS,
-            "data": successful,
-            "errors": errors,
-        }
-
-    except Exception as e:
-        return {
-            "success": False,
-            "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
-            "error": str(e),
-        }
-
+def process_batch_assertions(*args, **kwargs):
+    BadgeInstancesBatchAssertionTask.process_batch_assertions(*args, **kwargs)
 
 class BatchAssertionsIssue(VersionedObjectMixin, BaseEntityView):
     model = BadgeClass  # used by .get_object()
@@ -882,7 +884,6 @@ class BatchAssertionsIssue(VersionedObjectMixin, BaseEntityView):
             user_id=request.user.id,
             badgeclass_id=badgeclass.id,
             issuerSlug=issuerSlug,
-            serializer_class=self.v1_serializer_class,
             create_notification=create_notification,
         )
 
